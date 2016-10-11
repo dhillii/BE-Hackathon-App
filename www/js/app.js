11896fa5efbac5b4f8c2a4a07aa7e476d442672d
alannawalton <alanna.walton@bison.howard.edu>
2016-10-11 10:34:12 -0700

    newest headache

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,35 @@ var app = angular.module('myApp', [
     'ionic.native'
 ]);
 
+app.factory('idscanAPI', ['$http',
+    function($http) {
+        return {
+
+            //API from IDSCAN to parse ID barcodes
+            analyzeURL: function(image, callback) {
+                $http({
+                    method: 'GET',
+                    url:  'https://app1.idware.net/DriverLicenseParserRest.svc/ParseImage',
+                    headers: {
+                       'Content-Type': 'text/json',
+                       'Cache-Control': 'no-cache'
+                    },
+                    data: {
+                       "authKey": 'cd8594f1-df5f-4296-a155-92a977682923',
+                       "data": image
+                    },
+                }).then(function successCallback(result) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    callback(result);
+
+                })
+            }
+
+        
+    }
+}]);
+
 app.run(function($ionicPlatform, user) {
   // Initiate the user service with your UserApp App Id
   // https://help.userapp.io/customer/portal/articles/1322336-how-do-i-find-my-app-id-
@@ -35,10 +64,12 @@ app.run(function($ionicPlatform, user) {
 });
 
 //Controls initial landing page
-app.controller('mainCtrl', function($scope, $http, $location) {
-    //Changes the view/page
-    $scope.changeview = function(path) {
-        $location.path(path).replace();
+app.controller('mainCtrl', function($scope, $http, $location ) {
+    //Sidenav Functions
+    $scope.navOpen = false;
+    $scope.clickedNav = function(){
+        $scope.navOpen = !$scope.navOpen;
+        console.log($scope.navOpen);
     }
 });
 
@@ -65,8 +96,12 @@ app.controller('signupCtrl', function($scope, $http, $location, $cordovaCamera, 
       $cordovaCamera.getPicture().then(
         function(res) {
           console.log("We have taken a picture!", res);
-          $scope.DATA = "PICTURE UPLOADED!!"
           $scope.license = res;
+          idscanAPI.analyzeURL(res, function(data) {
+                console.log(data);
+            })
+          $scope.DATA = "PICTURE UPLOADED!!"
+          
         },
         function(err){
           console.error("Error taking a picture", err);
